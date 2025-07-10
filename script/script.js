bb7a144f1c3697109bdf360342b0cb8d19053873
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-07-10 16:29:19 -0300

    Refinamento do codigo js e correcao de bugs

--- script/script.js
+++ script/script.js
@@ -1,89 +1,42 @@
 console.log(menuOptions)
 
-const btnShowOptions = document.querySelector('#show-all')
-const btnAddAll = document.querySelector('#add-all')
-const btnVeganOptions = document.querySelector('#only-vegan')
-const btnDescontPrice = document.querySelector('#descont')
-const showValueAll = document.querySelector('.showValueAll')
-const foodCard = document.querySelector('ul');
+const show = document.querySelector('ul')
+const btnshowEverything = document.querySelector('#show-all')
+const btnMapAll = document.querySelector('#map-all')
 
+function showAll(arrayProducts) {
 
-function showOptions() {
+    show.innerHTML = ''
+    arrayProducts.forEach(element => {
 
-    foodCard.innerHTML = ''; // limpa os itens antigos
-
-    menuOptions.forEach((menuOptions, i) => {
-
-        foodCard.innerHTML += //exibe todos os itens do array
-
+        show.innerHTML +=
         `
-
         <li>
-            <img src="${menuOptions.src}" alt="burguer">
-            <p>${menuOptions.name}</p>
-            <p class="item-price">R$${menuOptions.price},00</p>
+            <img src="${element.src}" alt="burguer">
+            <p>${element.name}</p>
+            <p class="item-price">R$${element.price},00</p>
         </li>
-
         `
     });
-}
-
-function addAll() {
-    console.log('tá somando tudo')
-
-    const funcAdd = menuOptions.reduce((acc, item) => {
-        return acc + item.price
-    }, 0)
-
-    showValueAll.innerHTML = `O valor de todos os hamburgers é: R$${funcAdd},00`
-}
-
-function veganOptions() {
-
-    foodCard.innerHTML = ''; // limpa os itens antigos
-
-    const veganBurguer = menuOptions.filter(item => {
-        if (item.vegan === true) {
-
-            foodCard.innerHTML += //exibe todos os itens do array
-
-    `
-        <li>
-            <img src="${item.src}" alt="burguer">
-            <p>${item.name}</p>
-            <p class="item-price">R$${item.price},00</p>
-        </li>
-
-    `
-
-        }
-    })
-}
-
-function descontPrice() { // revisar
-
-    const foodCard = document.querySelector('ul');
-    foodCard.innerHTML = ''; // limpa os itens antigos
-
-    const porcent = menuOptions.map(item => {
-        const lowprice = item.price - (item.price / 10)
-        return lowprice
-    })
-
-     menuOptions.forEach((item, i) => {
-        foodCard.innerHTML += `
-            <li>
-                <img src="${item.src}" alt="burguer">
-                <p>${item.name}</p>
-                <p class="item-price">R$${porcent[i].toFixed(2)}</p>
-            </li>
-        `;
-    });
 
 }
 
-btnShowOptions.addEventListener('click', showOptions)
-btnAddAll.addEventListener('click', addAll)
-btnVeganOptions.addEventListener('click', veganOptions)
-btnDescontPrice.addEventListener('click', descontPrice)
+function mapAllitems() {
+
+    const descount = menuOptions.map(element => ({
+        ...element, //Spread Operator (matem todos os items de um array sem alteração)
+        price: element.price * 0.9
+    }))
+
+    showAll(descount);
+
+}
+
+btnshowEverything.addEventListener('click', () => showAll(menuOptions))
+btnMapAll.addEventListener('click', mapAllitems)
+
+
+
+
+
 
